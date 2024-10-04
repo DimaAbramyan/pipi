dbec1f92946b7bab5e945dbf3d455727bcb545c2
Егор Горобцов <egormiklas55@gmail.com>
2024-10-04 17:06:21 +0300

    Добавил решение задачи

---
+++ pipi/main.cpp
@@ -0,0 +1,12 @@
+#include "sum.h"
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	sum();
+
+	system("pause");
+	return 0;
+}
---
+++ pipi/sum.cpp
@@ -0,0 +1,13 @@
+#include "sum.h"
+#include <iostream>
+
+using namespace std;
+
+// Вычисляет сумму двух целых чисел
+void sum() {
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    cout << "Сумма: " << a + b << endl;
+}
